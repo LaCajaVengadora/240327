41fc058d1e5a078e1ffb75a92790214bc24f34e6
LaCajaVengadora_Local <schrodingercat06@gmail.com>
2024-03-27 18:32:38 -0300

    Added 4 apps to project & registered to settings.py

--- mercadoAutos/mercadoAutos/settings.py
+++ mercadoAutos/mercadoAutos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_app', 'email_app', 'profile_app', 'shop_app'
 ]
 
 MIDDLEWARE = [
