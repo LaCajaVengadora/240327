6c4d6db1371ac31065d39d36d02c0d2b830a5c10
LaCajaVengadora_Local <schrodingercat06@gmail.com>
2024-03-27 21:18:01 -0300

    Add & register 'items_app'

--- mercadoAutos/mercadoAutos/settings.py
+++ mercadoAutos/mercadoAutos/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'auth_app', 'email_app', 'profile_app', 'shop_app'
+    'auth_app', 'email_app', 'profile_app', 'shop_app', 'items_app'
 ]
 
 MIDDLEWARE = [
