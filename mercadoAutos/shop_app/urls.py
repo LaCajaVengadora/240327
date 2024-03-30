5c057f9aeb9dfcc51ab03ebe3a7a971665235859
LaCajaVengadora_Local <schrodingercat06@gmail.com>
2024-03-29 22:06:35 -0300

    Created Home, views with filter & mod styles (all shop_app)

--- mercadoAutos/shop_app/urls.py
+++ mercadoAutos/shop_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-from .views import test
+from .views import test, shop_view, cat_view
 urlpatterns = [
-    path('', test)
+    #path('', test)
+    path('', shop_view, name="Shop"),
+    path('<str:cat>', cat_view)
 ]
--- mercadoAutos/shop_app/views.py
+++ mercadoAutos/shop_app/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from items_app.models import Item, ItemImage, Category
 
 # Create your views here.
 def test(request): return render(request, 'base.html')
+
+def shop_view(request): return get_items(request)
+def cat_view(request, cat): return get_items(request, cat)
+
+def get_items(rq, cat = None):    
+
+    if cat is not None: paginator = Paginator(Item.objects.filter(category__name=cat).order_by('-price'), 12)
+    else: paginator = Paginator(Item.objects.all().order_by('-price'), 12)
+    # con Paginator, teniendo all items, divide cada 12 items por pag
+    pag_num = rq.GET.get('page', 1) # Obtiene num de pag del request, por defecto da 1
+    items = paginator.get_page(pag_num) # Devuelve la pag con sus items
+
+    cats = Category.objects.all()
+
+    return render(rq, 'home.html', {'items':items,'cats':cats})
