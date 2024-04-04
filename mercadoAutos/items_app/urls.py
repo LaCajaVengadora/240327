777b0ece51f87b040a54640b254cb782247be858
LaCajaVengadora_Local <schrodingercat06@gmail.com>
2024-04-04 19:36:35 -0300

    Added consultas on items_app

--- mercadoAutos/items_app/urls.py
+++ mercadoAutos/items_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import item_view, add_fav
+from .views import item_view, add_fav, ask_view
 urlpatterns = [
     path('<str:id>', item_view, name='Item'),
     path('addFav/<str:id>', add_fav, name='Fav'),
+    path('ask/', ask_view.as_view(), name='Ask')
 ] 
--- mercadoAutos/items_app/views.py
+++ mercadoAutos/items_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Item, FavoriteItem
 from django.contrib.auth.decorators import login_required
+from django.views.generic import View
+from django.core.mail import send_mail
+from django.conf.global_settings import EMAIL_HOST_USER
 
 # Create your views here.
 def item_view(request, id):
@@ -16,3 +19,18 @@ def add_fav(request, id):
     else: 
         FavoriteItem(user=user, item=item).save()
     return redirect(request.META.get('HTTP_REFERER'))
+
+class ask_view(View):
+    def get(self, request): return redirect('Shop')
+    #@login_required(login_url='/auth/login/')
+    def post(self, request):
+        offererEmail = request.POST.get('offererEmail'); consulta = request.POST.get('consulta')
+        if not request.user.is_authenticated: return redirect('Login')
+        send_mail(
+            f'{request.user.username} ha consultado por tu producto!',
+            f'{consulta}\n\nPuedes contactar a {request.user.username} en {request.user.email}',
+            EMAIL_HOST_USER,
+            [offererEmail],
+            fail_silently=False,
+        )
+        return render(request, 'success.html')
